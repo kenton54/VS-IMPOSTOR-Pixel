57624ea9d99cb616b5d3505828eef7fee91ce5d2
kentongus <kentogamerisaneko@gmail.com>
2025-08-21 21:45:31 -0400

    welp i tried :smiling_face_with_tear:

--- data/states/debug/weekSelector.hx
+++ data/states/debug/weekSelector.hx
@@ -1,6 +1,7 @@
 import funkin.backend.week.Week;
 import funkin.editors.EditorTreeMenu.EditorTreeMenuScreen;
 import funkin.options.type.IconOption;
+import funkin.options.type.NewOption;
 import funkin.options.type.TextOption;
 
 function create() {
@@ -27,8 +28,12 @@ function makeWeekOption(week:String):IconOption {
     return option;
 }
 
+var curWeek:Null<Dynamic> = null;
+
 function openWeekOption(weekData:Dynamic) {
-    var subMenu = new EditorTreeMenuScreen(weekData.name, "weekSelection.selectDifficulty");
+    var subMenu = new EditorTreeMenuScreen(weekData.name, "weekSelection.selectDifficulty", "weekSelection.", null, "newDifficulty", "newDifficultyDesc");
+
+    curWeek = weekData;
 
     for (difficulty in weekData.difficulties) {
         subMenu.add(new TextOption(difficulty, translate("weekSelection.acceptDifficulty"), "", () -> {
@@ -37,5 +42,59 @@ function openWeekOption(weekData:Dynamic) {
         }));
     }
 
+    subMenu.insert(0, new NewOption(translate('weekSelection.newDifficulty'), translate('weekSelection.newDifficultyDesc'), () -> {
+        openSubState(new WeekDificultyCreationScreen(saveDifficulty));
+    }));
+
+    subMenu.curSelected = 1;
+
     addMenu(subMenu);
+}
+
+function saveDifficulty(newDiff:String) {
+    curWeek.difficulties.push(newDiff);
+
+    // TODO: figure out a way to save changes
+}
+
+import funkin.editors.ui.UIButton;
+import funkin.editors.ui.UISubstateWindow;
+import funkin.editors.ui.UITextBox;
+import funkin.editors.ui.UIText;
+
+class WeekDificultyCreationScreen extends UISubstateWindow {
+    private var saveTo:Null<Dynamic> -> Void = null;
+
+    public var newDiffTxtBox:UITextBox;
+
+    public var saveButton:UIButton;
+	public var closeButton:UIButton;
+
+    public function new(saveTo:(String)->Void) {
+        super();
+        this.saveTo = (saveTo != null) ? saveTo : null;
+    }
+
+    override public function create() {
+        winTitle = translate("weekDifficultyCreationScreen.win-title");
+
+        winWidth = 360;
+		winHeight = 220;
+
+        super.create();
+
+        var diffTxt:UIText;
+		add(diffTxt = new UIText(windowSpr.x + 20, windowSpr.y + 30 + 16, 0, translate("weekDifficultyCreationScreen.title"), 28));
+
+        add(newDiffTxtBox = new UITextBox(diffTxt.x, diffTxt.y + diffTxt.height + 36, translate("weekDifficultyCreationScreen.diffName")));
+
+        add(saveButton = new UIButton(windowSpr.x + windowSpr.bWidth - 20 - 125, windowSpr.y + windowSpr.bHeight - 16 - 32, translate("editor.saveClose"), function() {
+			saveNewDiff();
+			close();
+		}, 125));
+    }
+
+    private function saveNewDiff() {
+		if (saveTo != null) saveTo(newDiffTxtBox.label.text);
+	}
 }
