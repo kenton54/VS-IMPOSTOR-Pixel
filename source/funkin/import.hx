6d6f604d7eee7c0e23000af26134884667ec3345
kenton54 <kentogamerisaneko@gmail.com>
2026-09-18 00:11:36 -0300

    fixed something stupid

---
+++ source/funkin/import.hx
@@ -0,0 +1,38 @@
+package funkin;
+
+#if !macro
+#if FEATURE_DISCORD_API
+import funkin.api.DiscordRPC;
+#end
+
+import flixel.FlxCamera;
+import flixel.FlxG;
+import flixel.group.FlxGroup;
+import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+
+import funkin.Constants;
+import funkin.assets.Paths;
+import funkin.graphics.FunkinGroup;
+import funkin.graphics.FunkinSprite;
+import funkin.graphics.FunkinText;
+import funkin.input.Pointer;
+import funkin.input.Swipe;
+import funkin.sound.FunkinSound;
+import funkin.system.Conductor;
+import funkin.utils.MathUtil;
+
+import openfl.utils.Assets;
+
+using Lambda;
+using StringTools;
+
+using funkin.utils.tools.FloatTools;
+using funkin.utils.tools.IntTools;
+using funkin.utils.tools.StringTools;
+
+using thx.Arrays;
+#end
---
+++ source/funkin/menus/import.hx
@@ -0,0 +1,11 @@
+package funkin.menus;
+
+#if !macro
+import funkin.input.InputManager;
+import funkin.menus.mainmenu.MainMenuState;
+import funkin.ui.BackButton;
+import funkin.ui.MusicBeatState;
+import funkin.ui.StarsBackdrop;
+import funkin.ui.StaticButton;
+import funkin.ui.transitions.*;
+#end
